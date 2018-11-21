28b32002b3365ec80132a75051726d5c2aca02ba
Efrain <efrain@sg3dev.tk>
2018-11-21 19:47:11 +0000

    First working version

--- lib/http_client.ex
+++ lib/http_client.ex
@@ -4,15 +4,56 @@ defmodule HttpClient do
   """
 
   @doc """
-  Hello world.
+  Make an http "GET" request to the specified url. The url should have
+  a specified protocol (http or https).
 
-  ## Examples
-
-      iex> HttpClient.hello()
-      :world
+  ```
+  > import HttpClient
+  > url = URI.parse "http://example.com"
+  > HttpClient.get url
+  {:ok, 200, "..."}
+  ``` 
 
   """
-  def hello do
-    :world
+  @spec get(URI.t) :: {:ok, String.t} | {:error, String.t}
+  def get(%URI{} = url), do: get(url, %URI{})
+  def get(_), do: {:error, "Invalid url. Use %URI{} structs."}
+  
+  @doc """
+  Make an http "GET"request to the specified url using the given proxy. The url should have a specified protocol (http or https).
+
+  ```
+  > import HttpClient
+  > url = URI.parse "http://example.com"
+  > proxy = URI.parse "123.456.789.1:8080"
+  > HttpClient.get url, proxy
+  {:ok, 200, "..."}
+  ```
+
+  """
+  @spec get(URI.t, URI.t) :: {:ok, String.t} | {:error, String.t}
+  def get(%URI{} = url, %URI{} = proxy) do
+    case url.host do
+      nil ->
+	{:error, "Invalid url or proxy. Use %URI{} structs for both."}
+	
+      _ ->
+	headers = []
+	options = [proxy: URI.to_string(proxy), hacknet: [:insecure]]
+
+	url
+        |> URI.to_string()
+        |> HTTPoison.get(headers, options)
+        |> response()
+    end
+  end
+  def get(_, _), do: {:error, "Invalid url or proxy. Use %URI{} structs for both."}
+
+  defp response({:ok, %HTTPoison.Response{} = resp}) do
+    {:ok, resp.status_code, resp.body}
+  end
+
+  defp response({:error, %HTTPoison.Error{} = err}) do
+    {:error, "HTTPoison error: #{inspect(err.reason)}"}
   end
 end
--- test/http_client_test.exs
+++ test/http_client_test.exs
@@ -2,7 +2,18 @@ defmodule HttpClientTest do
   use ExUnit.Case
   doctest HttpClient
 
-  test "greets the world" do
-    assert HttpClient.hello() == :world
+  test "make a request to the provided url" do
+    {:ok, resp} =  HTTPoison.get("http://example.com")
+    url = URI.parse("http://example.com")
+    assert HttpClient.get(url) == {:ok, resp.status_code, resp.body}
+    assert HttpClient.get("") == {:error, "Invalid url. Use %URI{} structs."}
+  end
+
+  test "make a request to the provided url witht he given proxy" do
+    {:ok, resp} = HTTPoison.get("http://example.com")
+    url = URI.parse("http://example.com")
+    proxy = URI.parse(%URI{})
+    assert HttpClient.get(url, proxy) == {:ok, resp.status_code, resp.body}
+    assert HttpClient.get(url, "") == {:error, "Invalid url or proxy. Use %URI{} structs for both."}
   end
 end
